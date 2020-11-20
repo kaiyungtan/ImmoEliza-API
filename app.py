30e932c265ff93268774723043f754b7496d18af
Tan Kai Yung <kaiyungtan2020@gmail.com>
2020-11-20 12:09:28 +0100

    flask-api

---
+++ app.py
@@ -0,0 +1,72 @@
+import numpy as np
+import pandas as pd
+from flask import Flask, request, jsonify, render_template, url_for, flash, redirect
+import joblib
+
+app = Flask(__name__)
+model_house = joblib.load('xgb_rs_model_house_20.11.2020.pkl')
+model_apartment = joblib.load('xgb_rs_model_apartment_20.11.2020.pkl')
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+@app.route("/house")
+def house():
+    return render_template("predict_house.html")
+
+@app.route("/apartment", methods = ['GET'])
+def apartment():
+    return render_template("predict_apartment.html")
+ 
+@app.route("/result")
+def result():
+    return render_template("result.html")
+
+
+@app.route('/predict_house',methods=['GET','POST'])
+def predict_house():
+    '''
+    For rendering results on HTML GUI
+    '''
+    columns = ['city_name', 'number_of_rooms', 'house_area', 'fully_equipped_kitchen',
+    'open_fire', 'terrace', 'garden', 'surface_of_the_land','number_of_facades', 
+    'swimming_pool', 'state_of_the_building','construction_year', 'province', 'region']
+
+    int_features = [x for x in request.form.values()]
+    int_features = np.array(int_features) 
+    int_features = int_features.reshape(-1,14)
+    final_features = pd.DataFrame(int_features,columns=columns)
+
+    prediction = model_house.predict(final_features)
+
+    output = round(prediction[0])
+
+    return render_template('result.html', prediction_text='Predicted price for the house is € {}'.format(output))
+
+ 
+@app.route('/predict_apartment',methods=['GET','POST'])
+def predict_apartment():
+    '''
+    For rendering results on HTML GUI
+
+    '''
+    columns = ['city_name', 'number_of_rooms', 'house_area', 'fully_equipped_kitchen',
+    'open_fire', 'terrace', 'garden', 'number_of_facades', 'swimming_pool', 
+    'state_of_the_building','construction_year', 'province', 'region']
+
+    int_features = [x for x in request.form.values()]
+    int_features = np.array(int_features) 
+    int_features = int_features.reshape(-1,13)
+    final_features = pd.DataFrame(int_features,columns=columns)
+
+    prediction = model_apartment.predict(final_features)
+
+    output = round(prediction[0])
+
+    return render_template('result.html', prediction_text='Predicted price for the apartment is € {}'.format(output))
+ 
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ request.py
@@ -0,0 +1,36 @@
+import requests
+
+url = 'http://localhost:5000/predict_house'
+r = requests.post(url,json={'city_name': 'Averbode',
+							'number_of_rooms': 4,
+							'house_area' : 226,
+							'surface_of_the_land' : 398,
+							'fully_equipped_kitchen': 1,
+							'open_fire':0,
+							'terrace': 1,
+							'garden': 1,
+							'number_of_facades': 3,
+							'swimming_pool': 0,
+							'state_of_the_building': 'to renovate',
+							'construction_year' : 1930,
+							'province': '	',
+							'region': 'Flandre'})
+
+print(r.json())
+
+url = 'http://localhost:5000/predict_apartment'
+r = requests.post(url,json={'city_name': 'Anderlecht',
+							'number_of_rooms': 2,
+							'house_area' : 130,
+							'fully_equipped_kitchen': 1,
+							'open_fire':0,
+							'terrace': 1,
+							'garden': 0,
+							'number_of_facades': 2,
+							'swimming_pool': 0,
+							'state_of_the_building': 'as new',
+							'construction_year' : 2017,
+							'province': 'Bruxelles-Capitale',
+							'region': 'Bruxelles'})
+
+print(r.json())
